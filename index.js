cf0263d9caf6d663b1639d38602a9797aa2f6089
Stephan de Vries <stephan281094@gmail.com>
2018-05-25 15:14:34 +0200

    Add clear command

--- index.js
+++ index.js
@@ -10,18 +10,25 @@ const formatMessage = (message, attachments = []) => ({
 })
 
 module.exports = async (req, res) => {
-  const { user_name: username } = await parse(req)
+  const { text, user_name: username } = await parse(req)
 
-  if (!username) send(res, 400, ':soccer: Bad request')
+  if (!username) {
+    return send(res, 400, ':soccer: Bad request')
+  }
+
+  if (text === 'clear') {
+    queue.clear()
+    return send(res, 200, formatMessage(`Queue has been cleared.`))
+  }
 
   queue.add(username)
   const players = Array.from(queue).join(', ')
 
   if (queue.size >= 4) {
     queue.clear()
-    send(res, 200, formatMessage(`${players} can play! :tada:`))
+    return send(res, 200, formatMessage(`${players} can play! :tada:`))
   } else {
-    send(res, 200, formatMessage(
+    return send(res, 200, formatMessage(
       `${username} has been added to the queue. [${queue.size}/4]` +
         (queue.size === 3 ? ' We need *one more*!' : ''),
       [ { text: `Players in queue: ${players}` } ]
