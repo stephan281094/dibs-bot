5651892e90fae5e4a96623080ccb04bce9115c3b
Stephan de Vries <stephan281094@gmail.com>
2018-05-31 16:30:29 +0200

    Add blacklist functionality

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+require('dotenv').config()
 const { send } = require('micro')
 const parse = require('urlencoded-body-parser')
 
 const queue = new Set()
+const blacklist = (process.env.BLACKLIST || '')
+  .split(',')
+  .map(entry => entry.trim())
+  .filter(entry => !!entry)
 
 const formatMessage = (message, attachments = []) => ({
   response_type: 'in_channel',
@@ -12,8 +17,9 @@ const formatMessage = (message, attachments = []) => ({
 module.exports = async (req, res) => {
   const { text, user_name: username } = await parse(req)
 
-  if (!username) {
-    return send(res, 400, ':soccer: Bad request')
+  if (!username) return send(res, 400, formatMessage('Bad request'))
+  if (blacklist.includes(username)) {
+    return send(res, 400, formatMessage('Nope. We won\'t let you! :smiling_imp:'))
   }
 
   if (text === 'clear') {
