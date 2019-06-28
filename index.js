bf0fa4c944369b2b41d7b99587cbb13cdfb0aade
Stephan de Vries <stephan281094@gmail.com>
2019-06-28 14:41:52 +0200

    Use Prettier standard formatting

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-require("dotenv").config()
-const { send } = require("micro")
-const parse = require("urlencoded-body-parser")
+require("dotenv").config();
+const { send } = require("micro");
+const parse = require("urlencoded-body-parser");
 
-const queue = new Set()
+const queue = new Set();
 const blacklist = (process.env.BLACKLIST || "")
   .split(",")
   .map(entry => entry.trim())
-  .filter(entry => !!entry)
+  .filter(entry => !!entry);
 
 const formatMessage = (message, attachments = []) => ({
   response_type: "in_channel",
   text: `:soccer: ${message}`,
   attachments
-})
+});
 
 module.exports = async (req, res) => {
-  const { text, token, user_name: username } = await parse(req)
+  const { text, token, user_name: username } = await parse(req);
 
   // Handle bad requests.
   if (token !== process.env.TOKEN || !username) {
-    return send(res, 400, formatMessage("Bad request"))
+    return send(res, 400, formatMessage("Bad request"));
   }
 
   // Deny people on the blacklist.
@@ -28,22 +28,22 @@ module.exports = async (req, res) => {
       res,
       200,
       formatMessage("Nope. We won't let you! :smiling_imp:")
-    )
+    );
   }
 
   // Clear the queue.
   if (text === "clear") {
-    queue.clear()
-    return send(res, 200, formatMessage(`Queue has been cleared.`))
+    queue.clear();
+    return send(res, 200, formatMessage(`Queue has been cleared.`));
   }
 
   // Add the user to the queue.
-  queue.add(username)
-  const players = Array.from(queue).join(", ")
+  queue.add(username);
+  const players = Array.from(queue).join(", ");
 
   if (queue.size >= 4) {
-    queue.clear()
-    return send(res, 200, formatMessage(`${players} can play! :tada:`))
+    queue.clear();
+    return send(res, 200, formatMessage(`${players} can play! :tada:`));
   } else {
     return send(
       res,
@@ -53,6 +53,6 @@ module.exports = async (req, res) => {
           (queue.size === 3 ? " We need *one more*!" : ""),
         [{ text: `Players in queue: ${players}` }]
       )
-    )
+    );
   }
-}
+};
