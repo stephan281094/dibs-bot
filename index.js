8129173752589aa7512cc02457f42f554ef75171
Stephan de Vries <stephan281094@gmail.com>
2019-06-28 16:31:32 +0200

    Persist queue in Redis

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 require("dotenv").config();
 const { send } = require("micro");
 const parse = require("urlencoded-body-parser");
+const redis = require("redis");
 
-const queue = new Set();
-const blacklist = (process.env.BLACKLIST || "")
-  .split(",")
-  .map(entry => entry.trim())
-  .filter(entry => !!entry);
-
+const QUEUE_KEY = "players";
 const formatMessage = (message, attachments = []) => ({
   response_type: "in_channel",
   text: `:soccer: ${message}`,
   attachments
 });
+
+const clearQueue = async client =>
+  new Promise(resolve => client.del(QUEUE_KEY, resolve));
+const add = async (client, user) =>
+  new Promise(resolve => client.rpush(QUEUE_KEY, user, resolve));
+const getQueue = async client =>
+  new Promise(resolve =>
+    client.lrange(QUEUE_KEY, 0, 4, (_err, values) => {
+      resolve(values);
+    })
+  );
 
 module.exports = async (req, res) => {
   const { text, token, user_name: username } = await parse(req);
@@ -22,36 +29,48 @@ module.exports = async (req, res) => {
     return send(res, 400, formatMessage("Bad request"));
   }
 
-  // Deny people on the blacklist.
-  if (blacklist.includes(username)) {
-    return send(
-      res,
-      200,
-      formatMessage("Nope. We won't let you! :smiling_imp:")
-    );
-  }
+  // Create Redis connection.
+  const client = redis.createClient(process.env.REDIS_HOST);
 
   // Clear the queue.
   if (text === "clear") {
-    queue.clear();
+    await clearQueue(client);
     return send(res, 200, formatMessage(`Queue has been cleared.`));
   }
 
-  // Add the user to the queue.
-  queue.add(username);
-  const players = Array.from(queue).join(", ");
+  // Check if the user is already in the queue.
+  let queue = await getQueue(client);
+  if (queue.includes(username)) {
+    return send(
+      res,
+      200,
+      formatMessage(
+        `${username} is already in the queue. [${queue.length}/4]` +
+          (queue.length === 3 ? " We need *one more*!" : ""),
+        [{ text: `Players in queue: ${queue.join(", ")}` }]
+      )
+    );
+  }
 
-  if (queue.size >= 4) {
-    queue.clear();
-    return send(res, 200, formatMessage(`${players} can play! :tada:`));
+  // Add the user to the queue.
+  await add(client, username);
+  queue = await getQueue(client);
+
+  if (queue.length >= 4) {
+    await clearQueue(client);
+    return send(
+      res,
+      200,
+      formatMessage(`${queue.join(", ")} can play! :tada:`)
+    );
   } else {
     return send(
       res,
       200,
       formatMessage(
-        `${username} has been added to the queue. [${queue.size}/4]` +
-          (queue.size === 3 ? " We need *one more*!" : ""),
-        [{ text: `Players in queue: ${players}` }]
+        `${username} has been added to the queue. [${queue.length}/4]` +
+          (queue.length === 3 ? " We need *one more*!" : ""),
+        [{ text: `Players in queue: ${queue.join(", ")}` }]
       )
     );
   }
